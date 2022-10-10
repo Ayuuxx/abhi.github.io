5f51a749afef51148af7c4ae638678e0687a8e73
Ayuuxx <113383597+Ayuuxx@users.noreply.github.com>
2022-10-10 10:43:35 +0530

    test

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+var prevScrollpos = window.scrollY;
+window.onscroll = function() 
+{
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("bottomnav").style.top = "0";
+  } else {
+    document.getElementById("bottomnav").style.top = "-65px";
+  }
+  prevScrollpos = currentScrollPos;
+}
